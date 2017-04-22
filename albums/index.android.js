5f55c26e3ee91414655f0b5f0ab7c0ad154ffa2f
uraniumreza <reza.uranium@gmail.com>
2017-04-23 01:38:10 +0600

    Albums Header

---
+++ albums/index.android.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {AppRegistry, Text} from 'react-native';
+import Header from './src/components/header'
+
+const App = () => {
+   return(
+      <Header />
+   );
+};
+
+AppRegistry.registerComponent('albums', () => App);
